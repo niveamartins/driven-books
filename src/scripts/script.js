d6098153ea1efc687bd94646bfbf18a0a598ecd0
Nivea <niveamartins@dcc.ufrj.br>
2021-07-30 12:32:24 -0300

    feature: not showing list when it is empty

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,7 +6,7 @@ function closeButtons(classToClose) {
     bookList.classList.remove("show");
     bookList.classList.add("hidden");
   } else {
-      closeBookInformation()
+    closeBookInformation();
   }
 }
 
@@ -21,6 +21,14 @@ function clearList() {
   listOfBooks = [];
   const showinBooks = document.querySelector(".listed-books");
   showinBooks.innerHTML = "";
+
+  const listButton = document.querySelector(".list-button");
+  listButton.disabled = true;
+  listButton.classList.add("disabled");
+
+  const bookList = document.querySelector(".books-list");
+  bookList.classList.remove("show");
+  bookList.classList.add("hidden");
 }
 
 function makeList() {
@@ -35,6 +43,10 @@ function addToList() {
   if (!checkIfBookAlreadyListed(bookString)) {
     listOfBooks.push(bookString);
   }
+
+  const listButton = document.querySelector(".list-button");
+  listButton.disabled = false;
+  listButton.classList.remove("disabled");
 }
 
 function getBookToList() {
@@ -53,89 +65,91 @@ function checkIfBookAlreadyListed(book) {
 }
 
 function showBookInformation(bookImg) {
-    const book = findBookInformation(bookImg)
+  const book = findBookInformation(bookImg);
 
-    document.querySelector(".book-title").innerText = book.title
-    document.querySelector(".book-author").innerText = book.author
-    document.querySelector(".book-details").innerText = book.description
+  document.querySelector(".book-title").innerText = book.title;
+  document.querySelector(".book-author").innerText = book.author;
+  document.querySelector(".book-details").innerText = book.description;
 }
 
 function selectBook(book) {
-    if (document.querySelector(".selected") !== null) {
-        document.querySelector(".selected").classList.remove("selected")
-    }
-    book.classList.add("selected")
-    const bookImg = book.querySelector(".book-img") 
-    showBookInformation(bookImg.src)
+  if (document.querySelector(".selected") !== null) {
+    document.querySelector(".selected").classList.remove("selected");
+  }
+  book.classList.add("selected");
+  const bookImg = book.querySelector(".book-img");
+  showBookInformation(bookImg.src);
 
-    const booksExhibition = document.querySelector(".books")
-    booksExhibition.classList.remove("books-without-selection")
-    booksExhibition.classList.add("books-with-selection")
+  const booksExhibition = document.querySelector(".books");
+  booksExhibition.classList.remove("books-without-selection");
+  booksExhibition.classList.add("books-with-selection");
 
-    const bookBox = document.querySelector(".book-box")
-    bookBox.classList.remove("hidden")
-
+  const bookBox = document.querySelector(".book-box");
+  bookBox.classList.remove("hidden");
 }
 
 function closeBookInformation() {
-    document.querySelector(".selected").classList.remove("selected")
-    
-    const booksExhibition = document.querySelector(".books")
-    booksExhibition.classList.remove("books-with-selection")
-    booksExhibition.classList.add("books-without-selection")
+  document.querySelector(".selected").classList.remove("selected");
 
-    const bookBox = document.querySelector(".book-box")
-    bookBox.classList.add("hidden")
+  const booksExhibition = document.querySelector(".books");
+  booksExhibition.classList.remove("books-with-selection");
+  booksExhibition.classList.add("books-without-selection");
+
+  const bookBox = document.querySelector(".book-box");
+  bookBox.classList.add("hidden");
 }
 
 function findBookInformation(bookImg) {
-    bookImg = bookImg.replace("http://127.0.0.1:5500", ".")
-    for (let bookIndex in booksInformation) {
-        if (booksInformation[bookIndex].img === bookImg){
-            return booksInformation[bookIndex]
-        }
+  bookImg = bookImg.replace("http://127.0.0.1:5500", ".");
+  for (let bookIndex in booksInformation) {
+    if (booksInformation[bookIndex].img === bookImg) {
+      return booksInformation[bookIndex];
     }
-    return false
+  }
+  return false;
 }
 
-
-
 const booksInformation = [
-    {
-      img: "./assets/img/a-garota-que-nao-se-calou.jpg",  
-      title: "A garota que Não Se Calou",
-      author: "Abi Daré",
-      description:
-        "Esta é a história inesquecível de uma menina que deseja estudar para poder encontrar sua voz e falar por si mesma. A garota que não se calou é uma narrativa comovente e triunfante sobre o poder de lutar pelos seus sonhos.",
-    },
-    {
-        img: "./assets/img/casamento-americano.jpg",
-        title: "Um Casamento Americano",
-        author: "Tayari Jones",
-        description: "Os recém-casados Celestial e Roy são a personificação do sonho americano e do empoderamento negro. Mas um dia os dois são separados por circunstâncias imprevisíveis: Roy é condenado a doze anos de prisão por um crime que Celestial sabe que ele não cometeu."
-    },
-    {   
-        img: "./assets/img/fique-comigo.jpg",
-        title: "Fique Comigo",
-        author: "Ayòbámi Adébáyò",
-        description: "Yejide está casada há quatro anos com Akin, mas ainda não engravidou. A família do marido espera avidamente por uma criança, uma vez que a tradição local está bastante ligada à continuidade da linhagem familiar. Por isso, eles pressionam Akin a aceitar uma nova esposa, embora ele e Yejide tenham concordado em serem monogâmicos."
-    },
-    {   
-        img: "./assets/img/minha-pele.jpg",
-        title: "Minha Pele",
-        author: "Lázaro Ramos",
-        description: "Movido pelo desejo de viver num mundo em que a pluralidade cultural, racial, étnica e social seja vista como um valor positivo, e não uma ameaça, Lázaro Ramos divide com o leitor suas reflexões sobre temas como ações afirmativas, gênero, família, empoderamento, afetividade e discriminação."
-    },
-    {   
-        img: "./assets/img/nascido-crime.jpg",
-        title: "Nascido do Crime",
-        author: "Trevor Noah",
-        description: "A jornada extraordinária de Trevor Noah ― da infância sob o apartheid, na África do Sul, à bancada de um dos mais importantes talk shows dos Estados Unidos ― começou com um ato criminoso: seu nascimento."
-    },
-    {   
-        img: "./assets/img/vermelho-branco-sangue-azul.jpg",
-        title: "Vermelho, Branco e Sangue Azul",
-        author: "Casey McQuiston",
-        description: "O que pode acontecer quando o filho da presidenta dos Estados Unidos se apaixona pelo príncipe da Inglaterra?"
-    }
-  ];
+  {
+    img: "./assets/img/a-garota-que-nao-se-calou.jpg",
+    title: "A garota que Não Se Calou",
+    author: "Abi Daré",
+    description:
+      "Esta é a história inesquecível de uma menina que deseja estudar para poder encontrar sua voz e falar por si mesma. A garota que não se calou é uma narrativa comovente e triunfante sobre o poder de lutar pelos seus sonhos.",
+  },
+  {
+    img: "./assets/img/casamento-americano.jpg",
+    title: "Um Casamento Americano",
+    author: "Tayari Jones",
+    description:
+      "Os recém-casados Celestial e Roy são a personificação do sonho americano e do empoderamento negro. Mas um dia os dois são separados por circunstâncias imprevisíveis: Roy é condenado a doze anos de prisão por um crime que Celestial sabe que ele não cometeu.",
+  },
+  {
+    img: "./assets/img/fique-comigo.jpg",
+    title: "Fique Comigo",
+    author: "Ayòbámi Adébáyò",
+    description:
+      "Yejide está casada há quatro anos com Akin, mas ainda não engravidou. A família do marido espera avidamente por uma criança, uma vez que a tradição local está bastante ligada à continuidade da linhagem familiar. Por isso, eles pressionam Akin a aceitar uma nova esposa, embora ele e Yejide tenham concordado em serem monogâmicos.",
+  },
+  {
+    img: "./assets/img/minha-pele.jpg",
+    title: "Minha Pele",
+    author: "Lázaro Ramos",
+    description:
+      "Movido pelo desejo de viver num mundo em que a pluralidade cultural, racial, étnica e social seja vista como um valor positivo, e não uma ameaça, Lázaro Ramos divide com o leitor suas reflexões sobre temas como ações afirmativas, gênero, família, empoderamento, afetividade e discriminação.",
+  },
+  {
+    img: "./assets/img/nascido-crime.jpg",
+    title: "Nascido do Crime",
+    author: "Trevor Noah",
+    description:
+      "A jornada extraordinária de Trevor Noah ― da infância sob o apartheid, na África do Sul, à bancada de um dos mais importantes talk shows dos Estados Unidos ― começou com um ato criminoso: seu nascimento.",
+  },
+  {
+    img: "./assets/img/vermelho-branco-sangue-azul.jpg",
+    title: "Vermelho, Branco e Sangue Azul",
+    author: "Casey McQuiston",
+    description:
+      "O que pode acontecer quando o filho da presidenta dos Estados Unidos se apaixona pelo príncipe da Inglaterra?",
+  },
+];
